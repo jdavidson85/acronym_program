04fde78bbd858c23ff869cfa284c01a62b1097f6
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-17 21:10:07 -0500

    Add files via upload

--- acronym_program.py
+++ acronym_program.py
@@ -1,12 +1,13 @@
-print("This programme will provide the acronym after inputting a phrase.")
+def main():
+    print("This programme will provide the acronym after inputting a phrase.")
+    phrase = input("Please enter a phrase to convert : ")
+    words = phrase.split()
+    acronym = ""
 
-phrase = input("Please enter a phrase to convert : ")
-list_of_words = phrase.split()
-add_acronym = ''
+    for word in words:
+        acronym += word[0]
+    acronym = acronym.upper()
+    print(acronym)
 
-for word in list_of_words:
-    add_acronym = add_acronym + word[0]
 
-new_acronym = add_acronym.upper()
-
-print(new_acronym)
+main()
